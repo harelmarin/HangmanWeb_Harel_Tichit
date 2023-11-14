b871be6120ced863d3e90a94b41a84f4767700fe
harelmarin <marin.harel@ynov.com>
2023-11-14 09:50:31 +0100

    mise en place serveur local

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func main() {
+	///// PARSER TEMPLATES HTML
+	temp, err := template.ParseGlob("./templates/*.html")
+	if err != nil {
+		fmt.Println(fmt.Sprint("ERREUR => %s", err.Error()))
+		return
+	}
+
+	//// 1ERE ROUTE
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		data := 0
+		temp.ExecuteTemplate(w, "test", data)
+	})
+
+	//// INIT DU SERVEUR LOCAL
+	rootDoc, _ := os.Getwd()
+	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
+
+	http.ListenAndServe("localhost:8080", nil)
+}
